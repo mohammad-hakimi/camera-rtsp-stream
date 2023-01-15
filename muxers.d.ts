a216987af3d53a7bf0a7f2f7c66b92f330ef54f5
Mohammad Hakimi <hakimi.mohammad.n@gmail.com>
2023-01-15 12:31:26 +0330

    UPDATE v0.2.18 change types

---
+++ muxers.d.ts
@@ -0,0 +1,24 @@
+
+declare module 'muxers' {
+    import {EventEmitter} from "events";
+    export default class Muxer extends EventEmitter {
+        constructor(
+            options: {
+                url: string;
+                ffmpegPath?: string;
+                ffmpegArgs?: { [key: string]: string };
+                timeout?: number;
+                format?: "mjpeg" | "mpeg1";
+                fps: number;
+                transport?: "tcp" | "udp"
+            },
+            type: "mjpeg" | "mpeg1"
+        )
+
+        stop(): void;
+
+        start(): void;
+
+        restart(): void;
+    }
+}
